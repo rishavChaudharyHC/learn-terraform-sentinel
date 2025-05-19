bc80f1a89c4ca074cfd62a1b0a3cee334309b2f3
rishavChaudharyHC <rishav.chaudhary@hashicorp.com>
2025-05-19 10:52:51 +0530

    changed output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "bucket_arn" {
+  value = aws_s3_bucket.my_bucket.arn
 }
