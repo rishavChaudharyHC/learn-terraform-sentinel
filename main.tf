0418e457a226074dde28a75c5c5e27d997a5a592
rishavChaudharyHC <rishav.chaudhary@hashicorp.com>
2025-05-19 11:54:39 +0530

    tags changed for test

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_s3_bucket" "sentinel_test_bucklet" {
   bucket = "random_bucket_for_sentinel_rishav"  # Replace with a globally unique name
 
   tags = {
-    Name        = "TestSentinelBucket"
     Environment = "Dev"
   }
 }
