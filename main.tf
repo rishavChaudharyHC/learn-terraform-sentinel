ee4022224e53ee82703b9f28440830889bdfcaa5
rishavChaudharyHC <rishav.chaudhary@hashicorp.com>
2025-05-19 10:49:13 +0530

    changed to create s3 bucket

--- main.tf
+++ main.tf
@@ -1,28 +1,12 @@
 provider "aws" {
-  region = var.region
+  region =var.region  
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
+resource "aws_s3_bucket" "sentinel_test_bucklet" {
+  bucket = "random_bucket_for_sentinel_rishav"  # Replace with a globally unique name
 
   tags = {
-    Name = var.instance_name
+    Name        = "TestSentinelBucket"
+    Environment = "Dev"
   }
 }
